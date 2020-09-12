3363c6ef9aa01586a218058ad928b730ffd84ddb
Siri <Siri@Siris-Air.fritz.box>
2020-09-12 14:24:11 +0200

    Changing subset

--- Basic_data_visuali_ggplot.R
+++ Basic_data_visuali_ggplot.R
@@ -1,16 +1,23 @@
 # loading data fram
 dat <- read.csv("datLexMidpoint_ST_slope.csv", sep = ",")
 
+
+datN <- dat[dat$Label == "n" & dat$SentenceType == "F_H" , ]
+datN <- droplevels(datN)
+
+write.csv(datN, "/Users/Siri/Documents/Coding/R/R_visualizations/subsetdataLexMidpointF0.csv")
+
+
 # visualizing factors: StartOfDrop vs.	TCoG
 library(ggplot2)
 
 # quick plot
-qplot(vowelOnsetF0, midpointF0, data = dat, color = Speaker)
+qplot(vowelOnsetF0, midpointF0, data = datN, color = Speaker)
 
 # Same visualization with ggplot specifying more options
 # Option for saving the file as .jpeg
 jpeg(filename = "MidpointF0.jpeg", height=1600, width=2000, res = 300)
-ggplot() + geom_point(data = dat, aes(x = vowelOnsetF0, y = midpointF0, color = Speaker)) +
+ggplot() + geom_point(data = datN, aes(x = vowelOnsetF0, y = midpointF0, color = Speaker)) +
   ggtitle("Midpoint of fundamental frequency")+
   theme(plot.title = element_text(hjust = 0.5))
 # command to close the plot
